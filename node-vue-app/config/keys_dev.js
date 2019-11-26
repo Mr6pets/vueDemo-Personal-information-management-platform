d90ab7b117d45b09f2b9e28946cfc0077283d608
liuan <1761330769@qq.com>
2019-11-26 11:38:31 +0800

    ..

---
+++ node-vue-app/config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // mongoURI: "mongodb://mern:abc123@ds127841.mlab.com:27841/restful-api-mern",
+  mongoURI: "mongodb+srv://PIMP:test1234@cluster0-rzg8n.mongodb.net/PIMP?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+}
